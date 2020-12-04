7508fab182ed7c0fd985438c2757b114ca99735e
Stef Rave <s.rave@nl.ccv.eu>
2020-12-04 08:37:50 +0100

    Day4

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -116,5 +116,60 @@ namespace AdventOfCode2020
             }
 
         }
+
+        [Fact]
+        public void DoDay4()
+        {
+            var validEyeColors = "amb blu brn gry grn hzl oth".Split(' ').ToHashSet();
+
+            var input = File.ReadAllText("input4.txt");
+
+            var candidates = Regex.Split(input, @"\r?\n\r?\n", RegexOptions.Singleline);
+            int resultPresent = 0;
+            int resultValidationOk = 0;
+            foreach (var candidate in candidates)
+            {
+                var keyValues = Regex.Matches(candidate, @"(\w+):(#?\w+)")
+                    .ToDictionary(r => r.Groups[1].Value, r => r.Groups[2].Value);
+
+                if (keyValues.TryGetValue("byr", out var byr) &&
+                    keyValues.TryGetValue("iyr", out var iyr) &&
+                    keyValues.TryGetValue("eyr", out var eyr) &&
+                    keyValues.TryGetValue("hgt", out var hgt) &&
+                    keyValues.TryGetValue("hcl", out var hcl) &&
+                    keyValues.TryGetValue("ecl", out var ecl) &&
+                    keyValues.TryGetValue("pid", out var pid))
+                {
+                    resultPresent++;
+
+                    if(ValidateNumberInRange(byr, 1920, 2002) &&
+                        ValidateNumberInRange(iyr, 2010, 2020) &&
+                        ValidateNumberInRange(eyr, 2020, 2030) &&
+                        ValidateHeight(hgt) &&
+                        Regex.Match(hcl, @"^#[0-9a-f]{6}$").Success &&
+                        validEyeColors.Contains(ecl) &&
+                        Regex.Match(pid, @"^\d{9}$").Success)
+                    {
+                        resultValidationOk++;
+                    }
+                }
+            }
+            output.WriteLine($"Part1: {resultPresent}");
+            output.WriteLine($"Part2: {resultValidationOk}");
+
+            bool ValidateNumberInRange(string val, int minIncl, int maxIncl) =>
+                int.TryParse(val, out int number) && number >= minIncl && number <= maxIncl;
+            bool ValidateHeight(string val)
+            {
+                string height = val[0..^2];
+
+                return val[^2..] switch
+                {
+                    "cm" => ValidateNumberInRange(height, 150, 193),
+                    "in" => ValidateNumberInRange(height, 59, 76),
+                    _ => false
+                };
+            }
+        }
     }
 }
