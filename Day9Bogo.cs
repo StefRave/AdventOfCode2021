5a8b3d85b7d3084fd0d18a0d70031b8cffbfdc83
Stef Rave <s.rave@nl.ccv.eu>
2020-12-09 16:46:56 +0100

    Day9Bogo Multi threaded

--- Day9Bogo.cs
+++ Day9Bogo.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -10,7 +11,6 @@ namespace AdventOfCode2020
     public class Day9Bogo
     {
         private readonly ITestOutputHelper output;
-        private Random rnd = new();
 
         public Day9Bogo(ITestOutputHelper output)
         {
@@ -31,29 +31,35 @@ namespace AdventOfCode2020
             (int offset, int length) = FindContiguousSetOfNumbers(result);
             long min = input.Skip(offset).Take(length).Min();
             long max = input.Skip(offset).Take(length).Max();
-            output.WriteLine($"Part2: {min+max}");
+            output.WriteLine($"Part2: {min + max}");
 
             long FindNumber(int sumOf)
             {
                 bool[] found = new bool[input.Length];
-                for(int i = 0; i < 10000000; i++)
+                int procCount = Environment.ProcessorCount;
+                var r = Parallel.For(0, procCount, x =>
                 {
-                    int start = rnd.Next(sumOf, input.Length);
-                    int offset1 = rnd.Next(0, sumOf - 1);
-                    int offset2 = rnd.Next(offset1 + 1, sumOf);
-                    found[start] |= input[start] == input[start - offset1 - 1] + input[start - offset2 - 1];
-                }
+                    Random rnd = new();
+                    for (int i = 0; i < 10000000 / 8; i++)
+                    {
+                        int start = rnd.Next(sumOf, input.Length);
+                        int offset1 = rnd.Next(0, sumOf - 1);
+                        int offset2 = rnd.Next(offset1 + 1, sumOf);
+                        found[start] |= input[start] == input[start - offset1 - 1] + input[start - offset2 - 1];
+                    }
+                });
                 return input[found.Select((b, i) => (b, i)).Single(c => c.i > sumOf && !c.b).i];
             }
 
             (int offset, int length) FindContiguousSetOfNumbers(long sumToBeFound)
             {
-                while(true)
+                Random rnd = new();
+                while (true)
                 {
                     int offset = rnd.Next(0, input.Length - 2);
-                    int end = rnd.Next(offset + 2, input.Length);
-                    if (input.Skip(offset).Take(end - offset).Sum() == sumToBeFound)
-                        return (offset, end - offset);
+                    int length = rnd.Next(2, Math.Min(input.Length - offset, 25));
+                    if (input.Skip(offset).Take(length).Sum() == sumToBeFound)
+                        return (offset, length);
                 }
             }
         }
