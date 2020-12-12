e8d15fe503bae732001c4f196f6cc5126699f8c4
Stef Rave <s.rave@nl.ccv.eu>
2020-12-12 09:38:08 +0100

    Day12 refactor

--- Day12.cs
+++ Day12.cs
@@ -19,32 +19,31 @@ namespace AdventOfCode2020
         public void DoDay12()
         {
             var input = File.ReadAllLines("input/input12.txt")
-                .Select(l => new Instr(l[0], int.Parse(l[1..])));
+                .Select(l => (instr: l[0], value: int.Parse(l[1..]);
 
             var ship = new Ship(0, 0, 0);
             foreach (var item in input)
-                ship = ship.Execute(item);
+                ship = ship.Execute(item.instr, item.value);
             output.WriteLine($"Part1: {ship.ManhattanDistance}");
 
             var shipWp = new ShipWaypoint(0, 0, 10, 1);
             foreach (var item in input)
-                shipWp = shipWp.Execute(item);
+                shipWp = shipWp.Execute(item.instr, item.value);
             output.WriteLine($"Part2: {shipWp.ManhattanDistance}");
         }
 
-        record Instr(char C, int Value);
         record Ship(int X, int Y, int Angle)
         {
-            public Ship Execute(Instr instr)
-                => instr.C switch
+            public Ship Execute(char instr, int value)
+                => instr switch
                 {
-                    'N' => this with { Y = Y + instr.Value },
-                    'E' => this with { X = X + instr.Value },
-                    'S' => this with { Y = Y - instr.Value },
-                    'W' => this with { X = X - instr.Value },
-                    'L' => this with { Angle = Angle - instr.Value },
-                    'R' => this with { Angle = Angle + instr.Value },
-                    'F' => this with { X = X + instr.Value * (int)Cos(Angle * PI / 180), Y = Y - instr.Value * (int)Sin(Angle * PI / 180) },
+                    'N' => this with { Y = Y + value },
+                    'E' => this with { X = X + value },
+                    'S' => this with { Y = Y - value },
+                    'W' => this with { X = X - value },
+                    'L' => this with { Angle = Angle - value },
+                    'R' => this with { Angle = Angle + value },
+                    'F' => this with { X = X + value * (int)Cos(Angle * PI / 180), Y = Y - value * (int)Sin(Angle * PI / 180) },
                 };
             public int ManhattanDistance
                 => Abs(X) + Abs(Y);
@@ -52,16 +51,16 @@ namespace AdventOfCode2020
 
         record ShipWaypoint(int X, int Y, int WX, int WY)
         {
-            public ShipWaypoint Execute(Instr instr)
-                => instr.C switch
+            public ShipWaypoint Execute(char instr, int value)
+                => instr switch
                 {
-                    'N' => this with { WY = WY + instr.Value },
-                    'E' => this with { WX = WX + instr.Value },
-                    'S' => this with { WY = WY - instr.Value },
-                    'W' => this with { WX = WX - instr.Value },
-                    'L' => ShipWaypointRotateWaypointRight(360 - instr.Value),
-                    'R' => ShipWaypointRotateWaypointRight(instr.Value),
-                    'F' => this with { X = X + instr.Value * WX, Y = Y + instr.Value * WY },
+                    'N' => this with { WY = WY + value },
+                    'E' => this with { WX = WX + value },
+                    'S' => this with { WY = WY - value },
+                    'W' => this with { WX = WX - value },
+                    'L' => ShipWaypointRotateWaypointRight(360 - value),
+                    'R' => ShipWaypointRotateWaypointRight(value),
+                    'F' => this with { X = X + value * WX, Y = Y + value * WY },
                 };
             public ShipWaypoint ShipWaypointRotateWaypointRight(int angle)
                 => angle switch
