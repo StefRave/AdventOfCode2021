fdb4fa1bbdd72fae3f0a6b8a5278b3acd7c54a7f
Stef Rave <stef.rave@gmail.com>
2020-12-25 10:26:56 +0100

    Day25

---
+++ Day25.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020
+{
+    public class Day25
+    {
+        private readonly ITestOutputHelper output;
+        public Day25(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void DoDay25()
+        {
+            var input = File.ReadAllLines("input/input25.txt");
+            int cardKey = int.Parse(input[0]);
+            int doorKey = int.Parse(input[1]);
+
+            long s = 1;
+            long result = 1;
+            do
+            {
+                s = s * 7 % 20201227;
+                result = result * doorKey % 20201227;
+            }
+            while (s != cardKey);
+
+            output.WriteLine($"Part1: {result}");
+        }
+    }
+}
