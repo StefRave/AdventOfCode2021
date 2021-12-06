d33fe70967fef2228bb15a96238e456e6de8f4fb
Stef Rave <stef.rave@gmail.com>
2021-12-06 08:27:26 +0100

    Day6

--- Day06.cs
+++ Day06.cs
@@ -17,13 +17,28 @@ namespace AdventOfCode2021
         [Fact]
         public void Run()
         {
-            var input = Advent.ReadInputLines()
+            var input = Advent.ReadInput().Split(',')
                 .Select(c => int.Parse(c))
                 .ToArray();
+            var numberOfFishOnCycle = new long[9];
+            foreach (var timerVal in input)
+                numberOfFishOnCycle[timerVal]++;
 
-            Advent.AssertAnswer1("answer1");
+            for (int i = 0; i < 256; i++)
+            {
+                if(i == 80)
+                    Advent.AssertAnswer1(numberOfFishOnCycle.Sum());
 
-            Advent.AssertAnswer2(2);
+                long newFishBorn = numberOfFishOnCycle[0];
+
+                for (int j = 1; j < numberOfFishOnCycle.Length; j++)
+                    numberOfFishOnCycle[j - 1] = numberOfFishOnCycle[j];
+
+                numberOfFishOnCycle[6] += newFishBorn;
+                numberOfFishOnCycle[^1] = newFishBorn;
+            }
+
+            Advent.AssertAnswer2(numberOfFishOnCycle.Sum());
         }
     }
 }
