7b7d77fac56f3edf80b479156e9d92c392843104
Stef Rave <stef.rave@gmail.com>
2021-12-08 14:37:08 +0100

    Day 8 Refactor

--- Day08.cs
+++ Day08.cs
@@ -20,54 +20,40 @@ public class Day08
         string[] Parse(string parts) => parts.Split(' ').Select(ToOrderedString).ToArray();
         string ToOrderedString(string s) => new string(s.OrderBy(a => a).ToArray());
 
-        int eight = input.Sum(line => line.output.Count(s => s.Length == 7));
-        int one = input.Sum(line => line.output.Count(s => s.Length == 2));
-        int four = input.Sum(line => line.output.Count(s => s.Length == 4));
-        int seven = input.Sum(line => line.output.Count(s => s.Length == 3));
-        Advent.AssertAnswer1(eight+one+four+seven);
+        var numbersToSum = new[] { 2, 4, 3, 7 };
+        int answer1 = input
+            .SelectMany(line => line.output)
+            .Count(s => numbersToSum.Contains(s.Length));
+        Advent.AssertAnswer1(answer1);
 
-        int answer2 = input.Sum(GetOutputValu);
+        int answer2 = input.Sum(line => GetOutputValue(line.signal, line.output));
         Advent.AssertAnswer2(answer2);
     }
 
-    private static int GetOutputValu((string[] signal, string[] output) line)
+    private static int GetOutputValue(string[] signal, string[] output)
     {
         string[] digits = new string[10];
-        digits[8] = line.signal.Single(s => s.Length == 7);
-        digits[1] = line.signal.Single(s => s.Length == 2);
-        digits[4] = line.signal.Single(s => s.Length == 4);
-        digits[7] = line.signal.Single(s => s.Length == 3);
+        var signals = signal.ToHashSet();
+        digits[8] = SelectAndRemove(7);
+        digits[1] = SelectAndRemove(2);
+        digits[4] = SelectAndRemove(4);
+        digits[7] = SelectAndRemove(3);
+        digits[9] = SelectAndRemove(6, s => HasAll(digits[7], s) && HasAll(digits[4], s));
+        digits[0] = SelectAndRemove(6, s => HasAll(digits[7], s));
+        digits[6] = SelectAndRemove(6);
+        digits[3] = SelectAndRemove(5, s => HasAll(digits[7], s));
+        digits[5] = SelectAndRemove(5, s => HasAll(s, digits[9]));
+        digits[2] = SelectAndRemove(5);
 
-        // 0 6
-        // 1 2
-        // 2 5
-        // 3 5
-        // 4 4
-        // 5 5
-        // 6 6
-        // 7 3
-        // 8 7
-        // 9 6        
-        var length6 = line.signal.Where(s => s.Length == 6).ToHashSet();
-        var length5 = line.signal.Where(s => s.Length == 5).ToHashSet();
-        digits[9] = SelectAndRemove(length6, s => HasAll(7, s) && HasAll(4, s));
-        digits[0] = SelectAndRemove(length6, s => HasAll(7, s));
-        digits[6] = length6.Single();
-        digits[3] = SelectAndRemove(length5, s => HasAll(7, s));
-        digits[5] = SelectAndRemove(length5, s => HasOneMissing(9, s));
-        digits[2] = length5.Single();
+        return output.Aggregate(0, (acc, val) => acc * 10 + Array.IndexOf(digits, val));
 
-        int outputNumber = 0;
-        foreach (var outputDigit in line.output)
-            outputNumber = outputNumber * 10 + Array.IndexOf(digits, outputDigit);
-        return outputNumber;
+        bool HasAll(string digit, string segment)
+            => !digit.Except(segment).Any();
 
-        bool HasAll(int digit, string segment) => digits[digit].All(d => segment.Contains(d));
-        bool HasOneMissing(int digit, string segment) => digits[digit].Count(d => !segment.Contains(d)) == 1;
-        string SelectAndRemove(HashSet<string> options, Func<string, bool> predicate)
+        string SelectAndRemove(int numberOfSegments, Func<string, bool> predicate = null)
         {
-            string result = options.Single(predicate);
-            options.Remove(result);
+            string result = signals.Single(o => o.Length == numberOfSegments && (predicate == null || predicate(o)));
+            signals.Remove(result);
             return result;
         }
     }
