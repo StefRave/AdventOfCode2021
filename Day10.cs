d0b3c68f57a746c174ec88bf1cf650e75b96cf34
Stef Rave <stef.rave@gmail.com>
2021-12-10 08:45:00 +0100

    Day10

--- Day10.cs
+++ Day10.cs
@@ -4,20 +4,55 @@ public class Day10
 {
     private readonly ITestOutputHelper output;
 
-    public Day10(ITestOutputHelper output)
+    record Day10Data(char Open, char Close, int ScoreClose, int ScoreCorrupt);
+    private static Day10Data[] day10data = new[]
     {
-        this.output = output;
-    }
+        new Day10Data('(', ')', 1, 3),
+        new Day10Data('[', ']', 2, 57),
+        new Day10Data('{', '}', 3, 1197),
+        new Day10Data('<', '>', 4, 25137),
+    };
+    private static Dictionary<char, Day10Data> dataPerOpenChar = day10data.ToDictionary(x => x.Open);
 
     [Fact]
     public void Run()
     {
-        var input = Advent.ReadInputLines()
-            .Select(c => int.Parse(c))
-            .ToArray();
+        var input = Advent.ReadInputLines();
+        int totalCorruptPoints = 0;
+        var lineScores = new List<long>();
 
-        Advent.AssertAnswer1("answer1");
+        foreach (var line in input)
+        {
+            var stack = new Stack<Day10Data>();
+            bool error = false;
 
-        Advent.AssertAnswer2(2);
+            foreach (char c in line)
+            {
+                bool isOpen = dataPerOpenChar.TryGetValue(c, out var data);
+                if (isOpen)
+                    stack.Push(data);
+                else
+                {
+                    data = stack.Pop();
+                    if (c != data.Close)
+                    {
+                        totalCorruptPoints += day10data.First(d => d.Close == c).ScoreCorrupt;
+                        error = true;
+                        break;
+                    }
+                }
+            }
+            if (!error && stack.Count > 0)
+            {
+                long totalLineScore = 0;
+                foreach (var day10Data in stack)
+                    totalLineScore = totalLineScore * 5 + day10Data.ScoreClose;
+                lineScores.Add(totalLineScore);
+            }
+        }
+        Advent.AssertAnswer1(totalCorruptPoints);
+
+        lineScores.Sort();
+        Advent.AssertAnswer2(lineScores[lineScores.Count / 2]);
     }
 }
