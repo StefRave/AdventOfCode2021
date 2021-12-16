086c3336a87315ae858b3dd8b1acaa738ae7cc86
Stef Rave <stef.rave@gmail.com>
2021-12-16 08:50:50 +0100

    Day16

--- Day16.cs
+++ Day16.cs
@@ -12,12 +12,97 @@ public class Day16
     [Fact]
     public void Run()
     {
-        var input = Advent.ReadInputLines()
-            .Select(c => int.Parse(c))
-            .ToArray();
+        var input = Advent.ReadInput();
+        var parser = new Parser(input);
 
-        Advent.AssertAnswer1("answer1");
+        int totalV = 0;
+        long result2 = ReadPacket();
+        Advent.AssertAnswer1(totalV);
+        Advent.AssertAnswer2(result2);
 
-        Advent.AssertAnswer2(2);
+
+        long ReadPacket()
+        {
+            int v = parser.GetBits(3);
+            totalV += v;
+            int t = parser.GetBits(3);
+
+            if (t == 4)
+            {
+                long result = 0;
+                while (true)
+                {
+                    int g = parser.GetBits(5);
+                    result = (result << 4) + (g & 0xf);
+                    if ((g & 0x10) == 0)
+                        return result;
+                }
+            }
+            var values = new List<long>();
+            int lengthType = parser.GetBits(1);
+            if (lengthType == 0)
+            {
+                int subPacketBitsLength = parser.GetBits(15);
+                int readUntil = parser.Position + subPacketBitsLength;
+                while (parser.Position < readUntil)
+                    values.Add(ReadPacket());
+            }
+            else
+            {
+                int numberOfSubPackets = parser.GetBits(11);
+                for (int i = 0; i < numberOfSubPackets; i++)
+                    values.Add(ReadPacket());
+            }
+            return t switch
+            {
+                0 => values.Sum(),
+                1 => values.Aggregate(1L, (acc, val) => acc * val),
+                2 => values.Aggregate(long.MaxValue, (acc, val) => Math.Min(acc, val)),
+                3 => values.Aggregate(long.MinValue, (acc, val) => Math.Max(acc, val)),
+                5 => values[0] > values[1] ? 1 : 0,
+                6 => values[0] < values[1] ? 1 : 0,
+                7 => values[0] == values[1] ? 1 : 0,
+                _ => throw new InvalidOperationException($"unexpected {t}")
+            };
+        }
+    }
+
+    public class Parser
+    {
+        private string input;
+        int index = 0;
+        int bits;
+        int bitsLeft;
+
+        public bool IsEndOfInput => input.Length == index && bitsLeft == 0;
+        public int Position => index * 4 - bitsLeft;
+
+        public Parser(string input) => this.input = input;
+
+        public int GetBits(int numberOfBitsNeeded)
+        {
+            int result = 0;
+            int bitsInResult = 0;
+            while (true)
+            {
+                int getBits = Math.Min(bitsLeft, Math.Min(4, numberOfBitsNeeded - bitsInResult));
+                result = (result << getBits) + (bits >> (bitsLeft - getBits));
+                bitsInResult += getBits;
+                bitsLeft -= getBits;
+                bits &= 0xf >> (4 - bitsLeft);
+                if (bitsInResult == numberOfBitsNeeded)
+                    break;
+
+                bits = Convert.ToInt32(input.Substring(index++, 1), 16);
+                bitsLeft = 4;
+            }
+            return result;
+        }
+
+        public void Clear()
+        {
+            bits = 0;
+            bitsLeft = 0;
+        }
     }
 }
