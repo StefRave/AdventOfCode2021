408b4b6e70ab0dab939df267d5f325482c3d89ce
Stef Rave <stef.rave@gmail.com>
2021-12-18 14:10:40 +0100

    Day 18 refactor

--- Day18.cs
+++ Day18.cs
@@ -15,104 +15,85 @@ public class Day18
     public void Run()
     {
         var input = Advent.ReadInputLines()
-            .Select(line => Parse(line))
+            .Select(line => Reduce(Parse(line)))
             .ToArray();
 
-        var prevLine = Reduce(input[0]);
-        foreach (var line in input.Skip(1))
-            prevLine = AddLines(prevLine, Reduce(line));
-        Advent.AssertAnswer1(Calc(prevLine));
+        var line = input.Aggregate((line1, line2) => AddAndReduce(line1, line2));
+        Advent.AssertAnswer1(Calc(line));
 
-        long max = 0;
-        for (int i = 0; i < input.Length; i++)
-            for (int j = 0; j < input.Length; j++)
-                if (i != j)
-                    max = Math.Max(max, Calc(AddLines(input[i], input[j])));
+        long max = input.Select(line1 => 
+            input.Select(line2 => Calc(AddAndReduce(line1, line2))).Max()).Max();
         Advent.AssertAnswer2(max);
     }
 
-    private static List<NumLevel> AddLines(List<NumLevel> prevLine, List<NumLevel> line)
+    private static List<NumLevel> AddAndReduce(List<NumLevel> lines1, List<NumLevel> lines2)
     {
-        var addedLine = new List<NumLevel>();
-        foreach (var numLevel in prevLine)
-            addedLine.Add(new NumLevel(numLevel.Num, numLevel.Level + 1, numLevel.CloseAfter));
-        foreach (var numLevel in line)
-            addedLine.Add(new NumLevel(numLevel.Num, numLevel.Level + 1, numLevel.CloseAfter));
-        addedLine[^1].CloseAfter++;
-        Reduce(addedLine);
-        return addedLine;
+        var result = lines1.Concat(lines2)
+            .Select(numLevel => new NumLevel(numLevel.Num, numLevel.Level + 1, numLevel.CloseAfter))
+            .ToList();
+        result[^1].CloseAfter++;
+        return Reduce(result);
     }
 
-    private static long Calc(List<NumLevel> parsedLine)
+    private static long Calc(List<NumLevel> list)
     {
-        while (parsedLine.Count > 1)
-        {
-            bool modified = false;
-            for (int i = 0; i < parsedLine.Count - 1; i++)
-            {
-                if (parsedLine[i].Level != parsedLine[i + 1].Level || parsedLine[i].CloseAfter != 0)
-                    continue;
+        while (list.Count > 1)
+            for (int i = 0; i < list.Count - 1; i++)
+                if (list[i].IsPairWith(list[i + 1]))
+                    SumAndReplaceItemsAtIndex(i);
+        return list[0].Num;
 
-                if (i < parsedLine.Count - 1)
-                {
-                    parsedLine[i + 1].Num = parsedLine[i].Num  * 3 + parsedLine[i + 1].Num * 2;
-                    parsedLine[i + 1].Level--;
-                    parsedLine[i + 1].CloseAfter--;
-                    parsedLine.RemoveAt(i);
-                    modified = true;
-                    break;
-                }
-            }
-            if (!modified)
-                throw new Exception("No modification");
+        void SumAndReplaceItemsAtIndex(int i)
+        {
+            list[i] = new NumLevel(list[i].Num * 3 + list[i + 1].Num * 2, list[i + 1].Level - 1, list[i + 1].CloseAfter - 1);
+            list.RemoveAt(i + 1);
         }
-        return parsedLine[0].Num;
     }
 
     private static List<NumLevel> Reduce(List<NumLevel> parsedLine)
     {
-        while (true)
-        {
-            bool modified = false;
-            for (int i = 0; i < parsedLine.Count; i++)
-            {
-                if (parsedLine[i].Level == 5)
-                {
-                    if (parsedLine[i].CloseAfter != 0)
-                        throw new Exception();
-
-                    if (i > 0)
-                        parsedLine[i - 1].Num += parsedLine[i].Num;
-                    if (i < parsedLine.Count - 2)
-                        parsedLine[i + 2].Num += parsedLine[i + 1].Num;
-                    parsedLine.RemoveAt(i);
-                    parsedLine[i].Level--;
-                    parsedLine[i].Num = 0;
-                    parsedLine[i].CloseAfter--;
-                    modified = true;
-                    break;
-                }
-            }
-            if (!modified)
-            {
-                for (int i = 0; i < parsedLine.Count; i++)
-                {
-                    long num = parsedLine[i].Num;
-                    if (num >= 10)
-                    {
-                        parsedLine[i].Level++;
-                        parsedLine[i].CloseAfter++;
-                        parsedLine[i].Num = (num + 1) / 2;
-                        parsedLine.Insert(i, new NumLevel(num / 2, parsedLine[i].Level));
-                        modified = true;
-                        break;
-                    }
-                }
-            }
-            if (!modified)
-                break;
-        }
+        DoExplodes();
+        if (TrySplit())
+            return Reduce(parsedLine);
         return parsedLine;
+
+
+        void DoExplodes()
+        {
+            for (int i = 0; i < parsedLine.Count; i++)
+                if (parsedLine[i].Level == 5)
+                    Explode(i);
+        }
+
+        bool TrySplit()
+        {
+          for (int i = 0; i < parsedLine.Count; i++)
+                if (parsedLine[i].Num >= 10)
+                {
+                    Split(i);
+                    return true;
+                }
+            return false;
+        }
+
+        void Explode(int i)
+        {
+            if (i > 0)
+                parsedLine[i - 1].Num += parsedLine[i].Num;
+            if (i < parsedLine.Count - 2)
+                parsedLine[i + 2].Num += parsedLine[i + 1].Num;
+            parsedLine.RemoveAt(i);
+            parsedLine[i] = new NumLevel(0, parsedLine[i].Level - 1, parsedLine[i].CloseAfter - 1);
+        }
+
+        void Split(int i)
+        {
+            long num = parsedLine[i].Num;
+            parsedLine[i].Level++;
+            parsedLine[i].CloseAfter++;
+            parsedLine[i].Num = (num + 1) / 2;
+            parsedLine.Insert(i, new NumLevel(num / 2, parsedLine[i].Level));
+        }
     }
 
     private void Print(List<NumLevel> parsedLine)
@@ -140,21 +121,19 @@ public class Day18
 
     private static List<NumLevel> Parse(string line)
     {
-        var parsedLine = new List<NumLevel>();
-        NumLevel last = null;
+        var result = new List<NumLevel>();
         int level = 0;
         foreach (char c in line)
         {
             switch (c)
             {
                 case '[': level++; break;
-                case ']': level--; last.CloseAfter++; break;
+                case ']': level--; result[^1].CloseAfter++; break;
                 case ',': break;
-                default: last = new NumLevel(c - '0', level); parsedLine.Add(last); break;
+                default: result.Add(new NumLevel(c - '0', level)); break;
             }
         }
-
-        return parsedLine;
+        return result;
     }
 
     public class NumLevel
@@ -162,6 +141,8 @@ public class Day18
         public long Num { get; set; }
         public int Level { get; set; }
         public int CloseAfter { get; set; }
+
+        public bool IsPairWith(NumLevel other) => CloseAfter == 0 && Level == other.Level; 
 
         public NumLevel(long num, int level, int closeAfter = 0)
         {
