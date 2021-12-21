2628b190a39557441e3494df38f8676447580782
Stef Rave <stef.rave@gmail.com>
2021-12-21 12:47:48 +0100

    Day 21

--- Day21.cs
+++ Day21.cs
@@ -2,22 +2,49 @@ namespace AdventOfCode2021;
 
 public class Day21
 {
-    private readonly ITestOutputHelper output;
-
-    public Day21(ITestOutputHelper output)
-    {
-        this.output = output;
-    }
+    private static readonly int[] occurencesFor3Rolls = new int[] { 0, 0, 0, 1, 3, 6, 7, 6, 3, 1 };
 
     [Fact]
     public void Run()
     {
-        var input = Advent.ReadInputLines()
-            .Select(c => int.Parse(c))
+        var playerStartingPos = Advent.ReadInputLines()
+            .Select(line => new PosScore(int.Parse(line[^1..]), 0))
             .ToArray();
 
-        Advent.AssertAnswer1("answer1");
+        (int throws, int losingPlayerScore) = PlayNormal(playerStartingPos[0], playerStartingPos[1]);
+        Advent.AssertAnswer1(losingPlayerScore * throws);
 
-        Advent.AssertAnswer2(2);
+        var (player1Wins, player2Wins) = QuantumPlay(playerStartingPos[0], playerStartingPos[1]);
+        Advent.AssertAnswer2(Math.Max(player1Wins, player2Wins));
     }
+    private static (long playerAWins, long playerBWins) QuantumPlay(PosScore playerA, PosScore playerB)
+    {
+        long playerAWins = 0;
+        long playerBWins = 0;
+        for (int i = 3; i <= 9; i++)
+        {
+            var newPos = ((playerA.Pos + i - 1) % 10) + 1;
+            if (playerA.Score + newPos >= 21)
+                playerAWins += occurencesFor3Rolls[i];
+            else
+            {
+                var (wp2, wp1) = QuantumPlay(playerB, new PosScore(newPos, playerA.Score + newPos));
+                playerAWins += wp1 * occurencesFor3Rolls[i];
+                playerBWins += wp2 * occurencesFor3Rolls[i];
+            }
+        }
+        return (playerAWins, playerBWins);
+    }
+
+    private static (int throws, int losingPlayerScore) PlayNormal(PosScore playerA, PosScore playerB, int currentThrow = 0)
+    {
+        int moveSpaces = (++currentThrow + ++currentThrow + ++currentThrow) % 10;
+        int newPos = ((playerA.Pos + moveSpaces - 1) % 10) + 1;
+        int newScore = playerA.Score + newPos;
+        if (newScore >= 1000)
+            return (currentThrow, playerB.Score);
+        return PlayNormal(playerB, new PosScore(newPos, newScore), currentThrow);
+    }
+
+    public record PosScore(int Pos, int Score);
 }
