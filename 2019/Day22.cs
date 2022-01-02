950227cf8f61a4bb2118ebe83efe055528f2db84
Stef Rave <stef.rave@gmail.com>
2022-01-02 10:13:48 +0100

    Day 22 part 2 Fail

--- 2019/Day22.cs
+++ 2019/Day22.cs
@@ -1,5 +1,5 @@
 #nullable enable
-using FluentAssertions;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace AdventOfCode2019;
@@ -8,105 +8,80 @@ public class Day22 : IAdvent
 {
     private static string GetInput() => File.ReadAllText(@"Input/input22.txt");
 
-    [Fact]
-    public void Example1()
-    {
-        string input = @"deal with increment 7
-deal into new stack
-deal into new stack";
-        var commands = Parse(input);
-        Assert.Equal(3, commands.Length);
-        var stack = Enumerable.Range(0, 10).ToList();
-
-        stack = DealWithIncrement(stack, 7);
-    }
-
-    [Fact]
-    public void Example2()
-    {
-        string input = @"deal into new stack
-cut -2
-deal with increment 7
-cut 8
-cut -4
-deal with increment 7
-cut 3
-deal with increment 9
-deal with increment 3
-cut -1
-";
-        var commands = Parse(input);
-        var stack = Enumerable.Range(0, 10).ToList();
-
-        foreach (var command in commands)
-        {
-            stack = command switch
-            {
-                (true, 0) => DealIntoNewStack(stack),
-                (true, int num) => DealWithIncrement(stack, num),
-                (false, int num) => CutStack(stack, num),
-            };
-        }
-        stack.Should().BeEquivalentTo("9 2 5 8 1 4 7 0 3 6".Split(' ').Select(c => int.Parse(c)));
-    }
-
-    private List<int> CutStack(List<int> stack, int num)
-    {
-        if (num < 0)
-            num = stack.Count + num;
-        var newStack = new int[stack.Count];
-        stack.CopyTo(0, newStack, stack.Count - num, num);
-        stack.CopyTo(num, newStack, 0, stack.Count - num);
-
-        return new List<int>(newStack);
-    }
-
-    private List<int> DealIntoNewStack(List<int> stack)
-    {
-        for (int i = 0; i < stack.Count / 2; i++)
-        {
-            int tmp = stack[i];
-            stack[i] = stack[stack.Count - 1 - i];
-            stack[stack.Count - 1 - i] = tmp;
-        }
-        return stack;
-    }
-
-    private static List<int> DealWithIncrement(List<int> stack, int increment)
-    {
-        var newStack = new List<int>(new int[stack.Count]);
-        int length = stack.Count;
-        for (int i = 0; i < length; i++)
-            newStack[(i * increment) % length] = stack[i];
-        return newStack;
-    }
-
     void IAdvent.Run()
     {
-        // deal into new stack
-        // cut 9037
-        // deal with increment 49
-        // cut - 9932
-        // deal with increment 5
-        // cut 6434
         Line[] commands = Parse(GetInput());
 
-        Assert.Equal(GetInput().SplitByNewLine().Length, commands.Length);
+        Advent.AssertAnswer1(Execute(commands, 2019, 10007), 3377);
 
-        var stack = Enumerable.Range(0, 10007).ToList();
+        Process.GetCurrentProcess().Kill(); // FAIL to hard: Explenation is here https://codeforces.com/blog/entry/72593
 
+        long index = 2020;
+        long cardCount = 119315717514047;
+        for (long i = 0; i < cardCount; i++)
+        {
+            foreach (var command in commands.Reverse())
+            {
+                index = command switch
+                {
+                    (true, 0) => DealIntoNewStackR(index, cardCount),
+                    (true, int num) => DealWithIncrementR(index, cardCount, num),
+                    (false, int num) => CutStackR(index, cardCount, num),
+                };
+            }
+            WriteLine(index.ToString());
+            if (index == 2020)
+                1.ToString();
+        }
+        //Advent.AssertAnswer2(index, 2020);
+    }
+
+    private static long Execute(Line[] commands, long index, long cardCount)
+    {
         foreach (var command in commands)
         {
-            stack = command switch
+            index = command switch
             {
-                (true, 0) => DealIntoNewStack(stack),
-                (true, int num) => DealWithIncrement(stack, num),
-                (false, int num) => CutStack(stack, num),
+                (true, 0) => DealIntoNewStack(index, cardCount),
+                (true, int num) => DealWithIncrement(index, cardCount, num),
+                (false, int num) => CutStack(index, cardCount, num),
             };
         }
-        
-        Advent.AssertAnswer1(stack.IndexOf(2019), 0);
+        return index;
     }
+
+    private static long CutStack(long index, long cardCount, long num)
+        => (index + cardCount - num) % cardCount;
+
+    private static long DealIntoNewStack(long index, long cardCount)
+        => cardCount - index - 1;
+
+    private static long DealWithIncrement(long index, long cardCount, int increment)
+        => (index * increment) % cardCount;
+
+    private static long CutStackR(long index, long cardCount, long num)
+        => (index + cardCount + num) % cardCount;
+
+    private static long DealIntoNewStackR(long index, long cardCount)
+       => cardCount - index - 1;
+
+    private static long DealWithIncrementR(long index, long cardCount, int increment)
+        => (index * ModInverse(increment, cardCount)) % cardCount;
+
+    static long ModInverse(long a, long m)
+    {
+        long m0 = m;
+        long y = 0, x = 1;
+
+        if (m == 1)
+            return 0;
+        while (a > 1) // Euclid's algo
+            (m, a,x, y) = (a % m, m, y, x - (a / m) * y);
+        if (x < 0)
+            x += m0;
+        return x;
+    }
+
 
     private static Line[] Parse(string input)
     {
