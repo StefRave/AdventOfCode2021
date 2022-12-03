b87d81993bf6e53d0e487892dd37ef57a1b83bf0
Stef Rave <stef.rave@gmail.com>
2022-12-03 06:33:50 +0100

    Day 2022-3

--- 2022/Day03.cs
+++ 2022/Day03.cs
@@ -5,13 +5,24 @@ public class Day03 : IAdvent
     public void Run()
     {
         var input = Advent.ReadInputLines()
+            .Select(l => new[] { l.Substring(0, l.Length / 2), l.Substring(l.Length / 2) })
             .ToArray();
+        int answer1 = GetIntersectionReturnSum(input);
+        Advent.AssertAnswer1(answer1, expected: 7674, sampleExpected: 157);
 
-      
+        input = Advent.ReadInputLines()
+            .Chunk(3)
+            .ToArray();
+        int answer2 = GetIntersectionReturnSum(input);
+        Advent.AssertAnswer2(answer2, expected: 2805, sampleExpected: 70);
+    }
 
-        Advent.AssertAnswer1(10, expected: 0, sampleExpected: 0);
-
-     
-        Advent.AssertAnswer2(10, expected: 0, sampleExpected: 0);
+    private static int GetIntersectionReturnSum(string[][] input)
+    {
+        var intersect = input
+            .Select(l => l.Aggregate((a, b) => new string(a.Intersect(b).ToArray()))
+                .Select(c => c <= 'Z' ? (c - 'A' + 27) : (c - 'a' + 1)).ToArray())
+            .ToArray();
+        return intersect.Sum(l => l[0]);
     }
 }
