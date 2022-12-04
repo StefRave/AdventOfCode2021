8416aebf244d6d242b8f8d703495587cb3adf683
Stef Rave <stef.rave@gmail.com>
2022-12-04 06:40:02 +0100

    Day 2022-4

--- 2022/Day04.cs
+++ 2022/Day04.cs
@@ -1,17 +1,25 @@
+using System.ComponentModel;
+
 namespace AdventOfCode2022;
 
 public class Day04 : IAdvent
 {
     public void Run()
     {
-        var input = Advent.ReadInputLines()
+        (int from, int to)[][] input = Advent.ReadInputLines()
+            .Select(l => GetOrderedPairs(l.Split(',').Select(p => p.Split('-').Select(int.Parse).ToArray())))
             .ToArray();
 
-      
+        var fullyContains = input.Where(l => l[0].to >= l[1].to).Count();
+        Advent.AssertAnswer1(fullyContains, expected: 503, sampleExpected: 2);
 
-        Advent.AssertAnswer1(10, expected: 0, sampleExpected: 0);
-
-     
-        Advent.AssertAnswer2(10, expected: 0, sampleExpected: 0);
+        var overlap = input.Where(l => l[0].to >= l[1].from).Count();
+        Advent.AssertAnswer2(overlap, expected: 827, sampleExpected: 4);
     }
+
+    private static (int from, int to)[] GetOrderedPairs(IEnumerable<int[]> pairs) 
+        => pairs.Select(a => (from: a[0], to: a[1]))
+            .OrderBy(p => p.from)
+            .ThenByDescending(p => p.to)
+            .ToArray();
 }
