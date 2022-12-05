7c1de0ce4d8f14f7b51c903cad6ea888f92ac286
Stef Rave <stef.rave@gmail.com>
2022-12-05 08:32:19 +0100

    Day 2022-5

--- 2022/Day05.cs
+++ 2022/Day05.cs
@@ -4,14 +4,48 @@ public class Day05 : IAdvent
 {
     public void Run()
     {
-        var input = Advent.ReadInputLines()
+        var input = Advent.ReadInput()
+            .SplitByDoubleNewLine()
+            .ToArray();
+        var stackStartInput = input[0].SplitByNewLine();
+        var stackInput = new List<string>();
+        for (int i = 1; i < stackStartInput[0].Length; i += 4)
+        {
+            var line = stackStartInput.Select(line => line[i]).Reverse().ToArray();
+            stackInput.Add(new string(line).Trim());
+        }
+
+        var instructions = input[1].SplitByNewLine()
+            .Select(intstr =>
+            {
+                var r = Regex.Matches(intstr, @"\d+").Select(r => int.Parse(r.Captures[0].Value)).ToArray();
+                return (move: r[0], from: r[1], to: r[2]);
+            })
             .ToArray();
 
-      
+        string answer1 = Shuffle(moveMultiple: false);
+        Advent.AssertAnswer1(answer1, expected: "SPFMVDTZT", sampleExpected: "CMZ");
 
-        Advent.AssertAnswer1(10, expected: 0, sampleExpected: 0);
+        string answer2 = Shuffle(moveMultiple: true);
+        Advent.AssertAnswer2(answer2, expected: "ZFSJBPRFP", sampleExpected: "MCD");
 
-     
-        Advent.AssertAnswer2(10, expected: 0, sampleExpected: 0);
+
+        string Shuffle(bool moveMultiple)
+        {
+            var stacks = stackInput.Select(line => new Stack<char>(line)).ToArray();
+            foreach (var instr in instructions)
+            {
+                var tmp = moveMultiple ? new Stack<char>() : stacks[instr.to - 1];
+                for (int i = 0; i < instr.move; i++)
+                {
+                    var letter = stacks[instr.from - 1].Pop();
+                    tmp.Push(letter);
+                }
+                if (moveMultiple)
+                    foreach (var letter in tmp)
+                        stacks[instr.to - 1].Push(letter);
+            }
+            return new string(stacks.Select(s => s.Peek()).ToArray());
+        }
     }
 }
