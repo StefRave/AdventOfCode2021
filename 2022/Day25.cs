94354a35e3c3c7ca6bffa4d4b53f7a5d54b902af
Stef Rave <stef.rave@gmail.com>
2022-12-25 06:52:54 +0100

    Day 25

--- 2022/Day25.cs
+++ 2022/Day25.cs
@@ -1,17 +1,55 @@
+using System.Numerics;
+
 namespace AdventOfCode2022;
 
 public class Day25 : IAdvent
 {
     public void Run()
     {
-        var input = Advent.ReadInputLines()
-            .ToArray();
+        var input = Advent.ReadInputLines();
 
-      
+        long total = input.Sum(line => FromSnafu(line));
+        string answer1 = ToSnafu(total);
 
-        Advent.AssertAnswer1(10, expected: 1, sampleExpected: 0);
+        Advent.AssertAnswer1(answer1, expected: "2==221=-002=0-02-000", sampleExpected: "2=-1=0");
+    }
 
-     
-        Advent.AssertAnswer2(10, expected: 2, sampleExpected: 0);
+    private static long FromSnafu(string line)
+    {
+        long number = 0;
+        foreach (var c in line)
+        {
+            int d = c switch
+            {
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '-' => -1,
+                '=' => -2,
+                _ => throw new ArgumentOutOfRangeException(nameof(c))
+            };
+            number = 5 * number + d;
+        }
+        return number;
+    }
+
+    private static string ToSnafu(long number)
+    {
+        string result = "";
+        while (number != 0)
+        {
+            long current = ((number + 2) % 5) - 2;
+            string c = current switch
+            {
+                -2 => "=",
+                -1 => "-",
+                0 => "0",
+                1 => "1",
+                2 => "2",
+            };
+            result = c + result;
+            number = (number - current) / 5;
+        }
+        return result;
     }
 }
