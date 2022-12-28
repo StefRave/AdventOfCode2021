535a076cbd7d60a5b7e964ed5dd7b97910868380
Stef Rave <stef.rave@gmail.com>
2022-12-28 10:53:15 +0100

    Day 22 Refector some more

--- 2022/Day22.cs
+++ 2022/Day22.cs
@@ -6,11 +6,11 @@ public class Day22 : IAdvent
 {
     public record V2(int X, int Y)
     {
-        public static V2 operator -(V2 a, V2 b) => new V2(a.X - b.X, a.Y - b.Y);
-        public static V2 operator +(V2 a, V2 b) => new V2(a.X + b.X, a.Y + b.Y);
+        public static V2 operator -(V2 a, V2 b) => new (a.X - b.X, a.Y - b.Y);
+        public static V2 operator +(V2 a, V2 b) => new (a.X + b.X, a.Y + b.Y);
     }
-    static char[] Facing = new[] { '>', 'v', '<', '^' };
-    static V2[] MoveD = new V2[] { new V2(1, 0), new V2(0, 1), new V2(-1, 0), new V2(0, -1) };
+    static readonly char[] Facing = new[] { '>', 'v', '<', '^' };
+    static readonly V2[] MoveD = new V2[] { new (1, 0), new (0, 1), new (-1, 0), new (0, -1) };
     public void Run()
     {
         var input = Advent.ReadInput()
@@ -26,21 +26,21 @@ public class Day22 : IAdvent
         string[] instructions = Regex.Matches(input[1], @"\d+|\D+").Select(m => m.Value).ToArray();
 
         int maxY = maze.Select(m => m.Key.Y).Max() + 1;
-        var p = new V2(0, 0);
-        while (maze[p] != '.')
-            p = p + MoveD[0];
+        var startPoint = new V2(0, 0);
+        while (maze[startPoint] != '.')
+            startPoint += MoveD[0];
         int cubeSideSize = Math.Max(maxY, maxX) / 4;
 
 
-        int answer1 = DoIt(maze, instructions, p, part: 1);
+        int answer1 = DoIt(maze, instructions, startPoint, movePosition: MovePart1);
         Advent.AssertAnswer1(answer1, expected: 65368, sampleExpected: 6032);
 
 
-        int answer2 = DoIt(maze, instructions, p, part: 2);
+        int answer2 = DoIt(maze, instructions, startPoint, movePosition: MovePart2);
         Advent.AssertAnswer2(answer2, expected: 156166, sampleExpected: 5031);
 
 
-        int DoIt(Dictionary<V2, char> maze, string[] instructions, V2 p, int part)
+        int DoIt(Dictionary<V2, char> maze, string[] instructions, V2 p, Func<V2, int, (V2 pos, int facing)> movePosition)
         {
             int facing = 0;
             maze[p] = Facing[facing];
@@ -59,7 +59,7 @@ public class Day22 : IAdvent
                     for (int i = 0; i < moves; i++)
                     {
 
-                        (V2 newPos, int newFacing) = part == 1 ? MovePart1(p) : MovePart2(p, facing);
+                        (V2 newPos, int newFacing) = movePosition(p, facing);
                         if (newPos.Y < 0)
                             1.ToString();
                         if (maze[newPos] == '#')
@@ -79,87 +79,88 @@ public class Day22 : IAdvent
 
             int answer = 1000 * (p.Y + 1) + 4 * (p.X + 1) + facing;
             return answer;
-
-            (V2, int) MovePart2(V2 p, int facing)
-            {
-                V2 newPos = p;
-                newPos = p + MoveD[facing];
-                if (IsInMaze(newPos))
-                    return (newPos, facing);
-
-                return MoveNearEdge(p, facing);
-
-
-                (V2 p, int facing) MoveNearEdge(V2 p, int facing)
-                {
-                    var direction = new Vector3(1, 0, 0);
-                    var normal = new Vector3(0, 0, 1);
-                    var start = new Vector3(0, 0, 0);
-                    var pos = start;
-
-                    facing = (facing + 1) % 4;
-                    do
-                    {
-                        pos = pos + direction;
-
-                        int quadrantOld = p.Y / cubeSideSize * 10 + p.X / cubeSideSize;
-                        p = p + MoveD[facing];
-
-                        bool posIn = IsInMaze(p);
-                        bool trackIn = IsInMaze(p + MoveD[(facing + 3) % 4]);
-                        if (posIn && !trackIn)
-                        {
-                            int quadrantNew = p.Y / cubeSideSize * 10 + p.X / cubeSideSize;
-                            if (quadrantNew != quadrantOld)
-                            {
-                                (direction, normal) = CubeMoveStraightCorner(direction, normal);
-                                pos = pos + direction;
-                            }
-                        }
-                        else if (trackIn)
-                        {
-                            facing = (facing + 3) % 4;
-                            p = p + MoveD[facing];
-                            (direction, normal) = CubeMoveClosedCorner(direction, normal);
-                            pos = pos + direction;
-                        }
-                        else
-                        {
-                            p = p - MoveD[facing]; // move back
-                            facing = (facing + 1) % 4;
-                            (direction, normal) = CubeMoveOpenCorner(direction, normal);
-                            pos = pos + direction;
-                        }
-                    }
-                    while (pos != start);
-                    facing = (facing + 1) % 4;
-
-                    return (p, facing);
-                }
-            }
-
-            bool IsInMaze(V2 p) => (p.X >= 0) && (p.Y >= 0) && (p.X < maxX) && (p.Y < maxY) && maze[p] != ' ';
-             
-            (V2, int) MovePart1(V2 p)
-            {
-                V2 newPos = p;
-                do
-                {
-                    newPos = newPos + MoveD[facing];
-                    if (newPos.X < 0)
-                        newPos = new V2(maxX - 1, newPos.Y);
-                    else if (newPos.Y < 0)
-                        newPos = new V2(newPos.X, maxY - 1);
-                    else if (newPos.X >= maxX)
-                        newPos = new V2(0, newPos.Y);
-                    else if (newPos.Y >= maxY)
-                        newPos = new V2(newPos.X, 0);
-                }
-                while (maze[newPos] == ' ');
-                return (newPos, facing);
-            }
         }
+
+        (V2, int) MovePart1(V2 p, int facing)
+        {
+            V2 newPos = p;
+            do
+            {
+                newPos += MoveD[facing];
+                if (newPos.X < 0)
+                    newPos = new V2(maxX - 1, newPos.Y);
+                else if (newPos.Y < 0)
+                    newPos = new V2(newPos.X, maxY - 1);
+                else if (newPos.X >= maxX)
+                    newPos = new V2(0, newPos.Y);
+                else if (newPos.Y >= maxY)
+                    newPos = new V2(newPos.X, 0);
+            }
+            while (maze[newPos] == ' ');
+
+            return (newPos, facing);
+        }
+        
+        (V2, int) MovePart2(V2 p, int facing)
+        {
+            V2 newPos = p;
+            newPos = p + MoveD[facing];
+            if (IsInMaze(newPos))
+                return (newPos, facing);
+
+            return FindNewPositionByMoveNearEdge(p, facing);
+        }
+
+        (V2 p, int facing) FindNewPositionByMoveNearEdge(V2 p, int facing)
+        {
+            var direction = new Vector3(1, 0, 0);
+            var normal = new Vector3(0, 0, 1);
+            var start = new Vector3(0, 0, 0);
+            var pos = start;
+
+            facing = (facing + 1) % 4;
+            do
+            {
+                pos += direction;
+
+                int quadrantOld = p.Y / cubeSideSize * 10 + p.X / cubeSideSize;
+                p += MoveD[facing];
+
+                bool posIn = IsInMaze(p);
+                bool trackIn = IsInMaze(p + MoveD[(facing + 3) % 4]);
+                if (posIn && !trackIn)
+                {
+                    int quadrantNew = p.Y / cubeSideSize * 10 + p.X / cubeSideSize;
+                    if (quadrantNew != quadrantOld)
+                    {
+                        (direction, normal) = CubeMoveStraightCorner(direction, normal);
+                        pos += direction;
+                    }
+                }
+                else if (trackIn)
+                {
+                    facing = (facing + 3) % 4;
+                    p += MoveD[facing];
+                    (direction, normal) = CubeMoveClosedCorner(direction, normal);
+                    pos += direction;
+                }
+                else
+                {
+                    p -= MoveD[facing]; // move back
+                    facing = (facing + 1) % 4;
+                    (direction, normal) = CubeMoveOpenCorner(direction, normal);
+                    pos += direction;
+                }
+            }
+            while (pos != start);
+            facing = (facing + 1) % 4;
+
+            return (p, facing);
+        }
+
+        bool IsInMaze(V2 p) => (p.X >= 0) && (p.Y >= 0) && (p.X < maxX) && (p.Y < maxY) && maze[p] != ' ';
     }
+
 
     static (Vector3 m, Vector3 n) CubeMoveClosedCorner(Vector3 m, Vector3 n)
     {
@@ -192,12 +193,12 @@ public class Day22 : IAdvent
         public int Length() => X * X + Y * Y + Z * Z;
         public int ManhattanDistance() => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
 
-        public static Vector3 operator -(Vector3 a, Vector3 b) => new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
-        public static Vector3 operator +(Vector3 a, Vector3 b) => new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        public static Vector3 operator -(Vector3 a, Vector3 b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        public static Vector3 operator +(Vector3 a, Vector3 b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
 
         public static (int cos, int sin) GetRotationValues(int angle)
         {
-            angle = angle % 360;
+            angle %= 360;
             angle = angle < 0 ? angle + 360 : angle;
             return angle switch
             {
@@ -209,7 +210,7 @@ public class Day22 : IAdvent
             };
         }
 
-        public Vector3 Reverse() => new Vector3(-X, -Y, -Z);
+        public Vector3 Reverse() => new(-X, -Y, -Z);
 
         public Vector3 RotateX(int angle)
         {
