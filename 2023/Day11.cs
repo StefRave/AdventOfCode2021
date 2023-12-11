467194cb683e67d91fedf96a8afa0cfe4206384a
Stef Rave <stef.rave@gmail.com>
2023-12-11 09:39:15 +0100

    2023 Day11

--- 2023/Day11.cs
+++ 2023/Day11.cs
@@ -7,15 +7,9 @@ public class Day11 : IAdvent
         var input = Advent.ReadInput().SplitByNewLine()
             .Select(line => line.ToCharArray())
             .ToArray();
-        var markEmptyY = new bool[input.Length];
-        var markEmptyX = new bool[input[0].Length];
 
-        for (int y = 1; y < input.Length - 1; y++)
-            if (input[y].All(c => c == '.'))
-                markEmptyY[y] = true;
-        for (int x = 1; x < input[0].Length - 1; x++)
-            if (input.All(line => line[x] == '.' || line[x] == ' '))
-                markEmptyX[x] = true;
+        var markEmptyY = input.Select((_, y) => input[y].All(c => c == '.')).ToArray();
+        var markEmptyX = input[0].Select((_, x) => input.All(line => line[x] == '.')).ToArray();
 
         var coords = new List<(int X, int Y)>();
         for (int y = 0; y < input.Length; y++)
@@ -37,16 +31,20 @@ public class Day11 : IAdvent
             for (int i = 0; i < coords.Count; i++)
                 for (int j = i + 1; j < coords.Count; j++)
                 {
-                    var (cx1, cx2) = (coords[i].X, coords[j].X);
-                    var (cy1, cy2) = (coords[i].Y, coords[j].Y);
-                    var dx = (cx2 - cx1) == 0 ? 0 : (cx2 - cx1) / Math.Abs(cx2 - cx1);
-                    var dy = (cy2 - cy1) == 0 ? 0 : (cy2 - cy1) / Math.Abs(cy2 - cy1);
-                    for (int x = cx1; x != cx2; x += dx)
-                        totalDist += markEmptyX[x] ? distanceForEmptySpace : 1;
-                    for (int y = cy1; y != cy2; y += dy)
-                        totalDist += markEmptyY[y] ? distanceForEmptySpace : 1;
+                    AddDistance(coords[i].X, coords[j].X, markEmptyX);
+                    AddDistance(coords[i].Y, coords[j].Y, markEmptyY);
                 }
             return totalDist;
+
+
+            void AddDistance(int c1, int c2, bool[] markEmpty)
+            {
+                if (c1 == c2)
+                    return;
+                var dx = (c2 - c1) / Math.Abs(c2 - c1);
+                for (int x = c1; x != c2; x += dx)
+                    totalDist += markEmpty[x] ? distanceForEmptySpace : 1;
+            }
         }
     }
 }
