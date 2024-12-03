a78ba38a45f87070e92999a21e012b251f71675b
Stef Rave <stef.rave@gmail.com>
2024-12-03 18:41:16 +0100

    2024 Day03

--- 2024/Day03.cs
+++ 2024/Day03.cs
@@ -4,15 +4,34 @@ public class Day03 : IAdvent
 {
     public void Run()
     {
-        var input = Advent.ReadInput().SplitByNewLine();
+        var input = Advent.ReadInput();
 
-        int answer1 = 1;
+        long answer1 = Calc(input);
+        Advent.AssertAnswer1(answer1, expected: 185797128, sampleExpected: 161);
 
-        Advent.AssertAnswer1(answer1, expected: 11111, sampleExpected: 11111111);
+        long answer2 = Calc(input, dontSupport: true);
+        Advent.AssertAnswer2(answer2, expected: 89798695, sampleExpected: 48);
+    }
 
-        
-        int answer2 = 2;
-        Advent.AssertAnswer2(answer2, expected: 22222, sampleExpected: 22222222);
+    private static long Calc(string input, bool dontSupport = false)
+    {
+        MatchCollection matches = new Regex(@"(mul|do|don't)\((\d+,\d+|)\)", RegexOptions.Multiline).Matches(input);
+        bool doFlag = true;
+        long total = 0;
+        foreach (Match match in matches)
+        {
+            var opcode = match.Groups[1].Value;
+            if (opcode == "mul" && doFlag)
+            {
+                var args = match.Groups[2].Value.Split(',');
+                total += int.Parse(args[0]) * int.Parse(args[1]);
+            }
+            else if (opcode == "don't" && dontSupport)
+                doFlag = false;
+            else if (opcode == "do")
+                doFlag = true;
+        }
+        return total;
     }
 }
 
