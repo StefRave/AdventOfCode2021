e459e12a9baa73d88c0fb5d727c5c3e4116a267c
Stef Rave <stef.rave@gmail.com>
2024-12-23 14:51:44 +0100

    2024 Day 23 Optimize

--- 2024/Day23.cs
+++ 2024/Day23.cs
@@ -1,175 +1,102 @@
-
-using System.Diagnostics;
-using System.Linq;
-
 namespace AdventOfCode2024;
 
 public class Day23 : IAdvent
 {
+    static char ToChar(string s) => (char)((s[0] - 'a') * 64 + s[1] - 'a');
+    static string FromChar(char c) => new string([(char)(c / 64 + 'a'), (char)((c % 64) + 'a')]);
+
     public void Run()
     {
         var input = Advent.ReadInput().SplitByNewLine()
-            .Select(x => x.Split('-'))
+            .Select(x => x.Split('-').Select(ToChar).ToArray())
             .ToArray();
         var connections = BuildConnections(input);
 
         var setsOf3 = GetSetsOf3(connections);
-        int answer1 = setsOf3.Count(s => s.Nodes.Any(n => n.StartsWith("t")));
+        int answer1 = setsOf3.Count(s => s.Any(n => FromChar(n).StartsWith("t")));
         Advent.AssertAnswer1(answer1, expected: 1156, sampleExpected: 7);
 
         string answer2 = FindSetsWithMore(setsOf3);
         Advent.AssertAnswer2(answer2, expected: "bx,cx,dr,dx,is,jg,km,kt,li,lt,nh,uf,um", sampleExpected: "co,de,ka,ta");
 
 
-        string FindSetsWithMore(NodeSet[] sets)
+        string FindSetsWithMore(HashSet<string> sets)
         {
-            var solutions = new HashSet<NodeSet>();
-            for (int i = 0; i < sets.Length; i++)
+            var solutions = new HashSet<string>();
+            var setArray = sets.ToArray();
+            var removed = sets.ToDictionary(s => s, s => false);
+            for (int i = 0; i < setArray.Length; i++)
             {
-                var set = sets[i];
-                var candidates = set.Nodes.SelectMany(n => connections[n]).ToArray();
-                foreach (string candidate in candidates)
+                var set = setArray[i];
+                if (removed[set])
+                    continue;
+                removed[set] = true;
+                var candidates = set.SelectMany(n => connections[n]).ToArray();
+                foreach (char candidate in candidates)
                 {
                     var candidateConnections = connections[candidate];
-                    if (set.Nodes.All(n => candidateConnections.Contains(n)))
+                    if (set.All(n => candidateConnections.Contains(n)))
                     {
-                        solutions.Add(new NodeSet(set.Nodes.Concat([candidate])));
+                        string solution = AddSorted(set, candidate);
+                        solutions.Add(solution);
+                        for (int j = 0; j < solution.Length - 1; j++)
+                            removed[solution[..(j)] + solution[(j + 1)..]] = true;
                     }
                 }
             }
             if (solutions.Count > 1)
-                return FindSetsWithMore(solutions.ToArray());
-            return string.Join(",", solutions.First().Nodes);
+                return FindSetsWithMore(solutions);
+            return string.Join(",", solutions.First().Select(FromChar));
         }
     }
 
-    private static NodeSet[] GetSetsOf3(Dictionary<string, HashSet<string>> connections)
+    private static string AddSorted(string s, char c)
+        => Sort(s.Concat([c]));
+    private static string Sort(IEnumerable<char> s)
+        => new string(s.OrderBy(c => c).ToArray());
+
+    private static HashSet<string> GetSetsOf3(Dictionary<char, string> connections)
     {
-        var solutions = new HashSet<NodeSet>();
+        var solutions = new HashSet<string>();
         foreach (var (node, connected) in connections)
         {
-            foreach (string node2 in connected)
+            foreach (var node2 in connected)
             {
-                foreach (string node3 in connected)
+                foreach (var node3 in connected)
                 {
                     if (node2 == node3)
                         continue;
                     if (connections[node2].Contains(node3))
                     {
-                        if (solutions.Add(new NodeSet([node, node2, node3])))
-                        {
-                            //Console.WriteLine($"{node} {node2} {node3}");
-                        }
+                        solutions.Add(Sort([node, node2, node3]));
                     }
 
                 }
             }
         }
-        return solutions.ToArray();
+        return solutions;
     }
 
-    private static Dictionary<string, HashSet<string>> BuildConnections(string[][] input)
+    private static Dictionary<char, string> BuildConnections(char[][] input)
     {
-        var connections = new Dictionary<string, HashSet<string>>();
+        var connections = new Dictionary<char, HashSet<char>>();
 
         foreach (var line in input)
         {
-            connections.Update(line[0], v =>
+            Add(line[0], line[1]);
+            Add(line[1], line[0]);
+        }
+        return connections.ToDictionary(kv => kv.Key, kv => Sort(kv.Value));
+
+
+        void Add(char c1, char c2)
+        {
+            connections.Update(c1, v =>
             {
-                v ??= new HashSet<string>();
-                v.Add(line[1]);
+                v ??= new HashSet<char>();
+                v.Add(c2);
                 return v;
             });
-            connections.Update(line[1], v =>
-            {
-                v ??= new HashSet<string>();
-                v.Add(line[0]);
-                return v;
-            });
-        }
-        return connections;
-    }
-
-    [DebuggerDisplay("Nodes:{Nodes.Length} {string.Join(',', Nodes)}")]
-    public class NodeSet : IEquatable<NodeSet>
-    {
-        public string[] Nodes { get; }
-
-        public NodeSet(IEnumerable<string> nodes)
-            => Nodes = nodes.OrderBy(x => x).ToArray();
-
-        public override bool Equals(object obj) 
-            => Equals(obj as NodeSet);
-
-        public bool Equals(NodeSet other)
-        {
-            if (other == null)
-                return false;
-            if (Nodes.Length != other.Nodes.Length)
-                return false;
-            for (int i = 0; i < Nodes.Length; i++)
-                if (Nodes[i] != other.Nodes[i])
-                    return false;
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            int hashCode = 0;
-            foreach (var node in Nodes)
-                hashCode ^= node.GetHashCode();
-            return hashCode;
-        }
-
-        public bool Match(NodeSet set2)
-        {
-            string[] sortedStringsA = Nodes;
-            string[] sortedStringsB = set2.Nodes;
-            // count matches of sortedStringsA in sortedStringsB
-            int matches = 0;
-            int i = 0;
-            int j = 0;
-            while (i < sortedStringsA.Length && j < sortedStringsB.Length)
-            {
-                int compare = sortedStringsA[i].CompareTo(sortedStringsB[j]);
-                if (compare == 0)
-                {
-                    matches++;
-                    i++;
-                    j++;
-                }
-                else if (compare < 0)
-                {
-                    i++;
-                }
-                else
-                {
-                    j++;
-                }
-            }
-            return matches == Nodes.Length - 1;
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
