427e4a60c1c988468b1ebb8811bbb0b43c581352
Stef Rave <stef.rave@gmail.com>
2024-12-25 06:38:40 +0100

    2024 Day 25 🎉

--- 2024/Day25.cs
+++ 2024/Day25.cs
@@ -1,41 +1,37 @@
+using System.Linq;
+
 namespace AdventOfCode2024;
 
 public class Day25 : IAdvent
 {
     public void Run()
     {
-        var input = Advent.ReadInput().SplitByNewLine();
+        var input = Advent.ReadInput().SplitByDoubleNewLine();
+        var locks = new List<int[]>();
+        var keys = new List<int[]>();
 
-        int answer1 = 1;
-
-        Advent.AssertAnswer1(answer1, expected: 11111, sampleExpected: 11111111);
-
+        foreach (string inp in input)
+        {
+            var lines = inp.SplitByNewLine();
+            if (lines[0].All(c => c == '#'))
+                keys.Add(GetPattern(lines[1..]));
+            else if (lines[^1].All(c => c == '#'))
+                locks.Add(GetPattern(lines[..^1]));
+        }
         
-        int answer2 = 2;
-        Advent.AssertAnswer2(answer2, expected: 22222, sampleExpected: 22222222);
+        int answer1 = 
+            (
+                from int[] k in keys
+                from int[] l in locks
+                where k.Zip(l).All(a => a.First + a.Second <= 5)
+                select 1
+            ).Count();
+        Advent.AssertAnswer1(answer1, expected: 3284, sampleExpected: 3);
+    }
+    static int[] GetPattern(string[] lines)
+    {
+        return Enumerable.Range(0, lines[0].Length)
+            .Select(i => lines.Count(line => line[i] == '#'))
+            .ToArray();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
